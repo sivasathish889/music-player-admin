4c170ff2d03d3bd02b6ff62cfac1413137094561
sivasathish889 <sivasathish889@gmail.com>
2026-02-23 20:12:22 +0530

    api update

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: `${import.meta.env.VITE_API_BASE_URL}api` });
-
+const API = axios.create({ baseURL: "https://music-player-backends.onrender.com/api" });
 API.interceptors.request.use((config) => {
     const token = localStorage.getItem('admin_token');
     if (token) config.headers.Authorization = `Bearer ${token}`;
